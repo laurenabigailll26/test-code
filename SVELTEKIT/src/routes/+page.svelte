02b3d06daad3816e77cc19dffb41a6d47d6b6137
Lauren A <laurenabiii26@gmail.com>
2026-07-14 15:19:16 +0700

    testing sveltekit

---
+++ SVELTEKIT/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Testing SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    <button>Click me</button>
+</p>
+<p>Hi my name is {name}, i am trying to learn SvelteKit.</p>
